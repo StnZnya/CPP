22b611e29684420619094c8c856f3d19c26cee1c
StnZnya <sytnikzena8@gmail.com>
2023-10-24 20:49:40 +0300

    Added Lab_1

---
+++ Lab_1/1.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+#include <cmath> // math
+
+int main()
+{
+    int a = 2;
+    double b = 3.141;
+    double result;
+    result = cbrt((0.315 * log(a)) / (1 + cbrt((0.711 + log(b)) / (1 + cbrt(log(b/a))))));
+   
+    std::cout << result ;
+}
